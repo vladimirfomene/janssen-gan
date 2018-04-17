5891b338bc0b2953ba72c37b5ca702ec119b7c30
vladimirfomene <vladimirfomene@gmail.com>
2018-04-17 12:41:16 +0000

    Add printing to csv and create image files

--- train.py
+++ train.py
@@ -3,9 +3,11 @@ from torch.utils.data import DataLoader
 from torchvision import transforms, datasets
 import collections
 import hashlib
+import os
 import os.path
 import matplotlib.pyplot as plt
 import tensorflow as tf
+from skimage.io import imsave
 import re
 import numpy as np
 import torch
@@ -14,9 +16,10 @@ from tensorflow.python.framework import tensor_shape
 from tensorflow.python.platform import gfile
 from tensorflow.python.util import compat
 
+
 TRAIN_DATA_FOLDER = './tf_data/train_photos/'
 TEST_DATA_FOLDER = './tf_data/test_photos/'
-IMAGE_PIXELS = 28 * 28 * 3
+IMAGE_PIXELS = 28*28*3
 NOISE_SIZE = 10
 BATCH_SIZE = 10
 
@@ -24,12 +27,10 @@ BATCH_SIZE = 10
 def noise(n_rows, n_cols):
     return np.random.normal(size=(n_rows, n_cols))
 
-
 def xavier_init(size):
     in_dim = size[0] if len(size) == 1 else size[1]
     stddev = 1. / np.sqrt(float(in_dim))
     return tf.random_uniform(shape=size, minval=-stddev, maxval=stddev)
-
 
 def images_to_vectors(images):
     return images.reshape(images.shape[0], 2352)
@@ -43,26 +44,24 @@ def get_data(folder):
     compose = transforms.Compose(
         [transforms.ToTensor(),
          transforms.Normalize((.5, .5, .5), (.5, .5, .5))
-         ])
+        ])
     return datasets.ImageFolder(folder, transform=compose)
 
-
 def separate_data(data_loader):
-    negative_data = torch.FloatTensor([])
-    positive_data = torch.FloatTensor([])
+  negative_data = torch.FloatTensor([])
+  positive_data = torch.FloatTensor([])
+  
+  negative_counter = 0
+  positive_counter = 0
+  for n_batch, (batch, label) in enumerate(data_loader):
+    if(label[0] == 0):
+        negative_data = torch.cat((negative_data, batch))
+        negative_counter +=1
+    else:
+        positive_data = torch.cat((positive_data, batch))
+        positive_counter +=1
 
-    negative_counter = 0
-    positive_counter = 0
-    for n_batch, (batch, label) in enumerate(data_loader):
-        if (label[0] == 0):
-            negative_data = torch.cat((negative_data, batch))
-            negative_counter += 1
-        else:
-            positive_data = torch.cat((positive_data, batch))
-            positive_counter += 1
-
-    return negative_data.resize_((negative_counter, 10, 3, 28, 28)), positive_data.resize_(positive_counter, 10, 3, 28,
-                                                                                           28)
+  return negative_data.resize_((negative_counter, 10, 3, 28, 28)), positive_data.resize_(positive_counter, 10, 3, 28, 28)
 
 
 # Load train and test data
@@ -77,8 +76,13 @@ test_data_loader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=True)
 train_negatives, train_positives = separate_data(train_data_loader)
 test_negatives, test_positives = separate_data(test_data_loader)
 
+# print shape of test negatives and test positives
+#print(test_negatives.shape)
+#print(test_positives.shape)
+
 # Num batches
 num_batches = len(train_data_loader)
+
 
 ## Discriminator
 
@@ -104,6 +108,7 @@ D_B4 = tf.Variable(xavier_init([1]))
 # Store Variables in list
 D_var_list = [D_W1, D_B1, D_W2, D_B2, D_W3, D_B3, D_W4, D_B4]
 
+
 ## setting up a Generator network
 
 # Input
@@ -128,22 +133,19 @@ G_B4 = tf.Variable(xavier_init([2352]))
 # Store Variables in list
 G_var_list = [G_W1, G_B1, G_W2, G_B2, G_W3, G_B3, G_W4, G_B4]
 
-
 def discriminator(x):
-    l1 = tf.nn.dropout(tf.nn.leaky_relu(tf.matmul(x, D_W1) + D_B1, .2), .3)
-    l2 = tf.nn.dropout(tf.nn.leaky_relu(tf.matmul(l1, D_W2) + D_B2, .2), .3)
-    l3 = tf.nn.dropout(tf.nn.leaky_relu(tf.matmul(l2, D_W3) + D_B3, .2), .3)
+    l1 = tf.nn.dropout(tf.nn.leaky_relu(tf.matmul(x,   D_W1) + D_B1, .2), .3)
+    l2 = tf.nn.dropout(tf.nn.leaky_relu(tf.matmul(l1,  D_W2) + D_B2, .2), .3)
+    l3 = tf.nn.dropout(tf.nn.leaky_relu(tf.matmul(l2,  D_W3) + D_B3, .2), .3)
     out = tf.matmul(l3, D_W4) + D_B4
     return out
 
-
 def generator(z):
-    l1 = tf.nn.leaky_relu(tf.matmul(z, G_W1) + G_B1, .2)
+    l1 = tf.nn.leaky_relu(tf.matmul(z,  G_W1) + G_B1, .2)
     l2 = tf.nn.leaky_relu(tf.matmul(l1, G_W2) + G_B2, .2)
     l3 = tf.nn.leaky_relu(tf.matmul(l2, G_W3) + G_B3, .2)
     out = tf.nn.tanh(tf.matmul(l3, G_W4) + G_B4)
     return out
-
 
 G_sample = generator(Z)
 D_real = discriminator(X)
@@ -160,7 +162,7 @@ D_opt = tf.train.AdamOptimizer(2e-4).minimize(D_loss, var_list=D_var_list)
 G_opt = tf.train.AdamOptimizer(2e-4).minimize(G_loss, var_list=G_var_list)
 
 # Variable initialization
-num_epochs = 200
+num_epochs = 5000
 
 # Start interactive session
 session = tf.InteractiveSession()
@@ -170,20 +172,29 @@ tf.global_variables_initializer().run()
 
 # Init Logger
 # Needed to make sure the logging output is visible.
-# See https://github.com/tensorflow/tensorflow/issues/3047
+  # See https://github.com/tensorflow/tensorflow/issues/3047
 tf.logging.set_verbosity(tf.logging.INFO)
 
-# Training
+g_errors = []
+d_errors = []
+
+#Training
 # Iterate through epochs
-# Training
+#Training
 # Iterate through epochs
 for epoch in range(num_epochs):
-    for n_batch, batch in enumerate(train_positives):
+    print(epoch + "\n")
+    for n_batch, batch in enumerate(train_negatives):
 
         # 1. Train Discriminator
-        X_batch = images_to_vectors(batch.permute(0, 2, 3, 1).numpy())
-
-        Z_batch = images_to_vectors(train_negatives[n_batch].permute(0, 2, 3, 1).numpy())
+        if(n_batch == train_positives.shape[0]):
+            break
+        else:
+            X_batch = images_to_vectors(train_positives[n_batch].permute(0, 2, 3, 1).numpy())
+        
+    
+            
+        Z_batch = images_to_vectors(batch.permute(0, 2, 3, 1).numpy())
         feed_dict = {X: X_batch, Z: Z_batch}
         _, d_error, d_pred_real, d_pred_fake = session.run(
             [D_opt, D_loss, D_real, D_fake], feed_dict=feed_dict
@@ -195,21 +206,36 @@ for epoch in range(num_epochs):
             [G_opt, G_loss], feed_dict=feed_dict
         )
 
-        if n_batch % 100 == 0:
-            display.clear_output(True)
-            print(n_batch)
-            Z_batch_test = images_to_vectors(test_negatives[round(n_batch / 100)].permute(0, 2, 3, 1).numpy())
-            # Generate images from test noise
-            test_images = session.run(
-                G_sample, feed_dict={Z: Z_batch_test}
-            )
-            test_images = vectors_to_images(test_images)
-            # Log Images
-            for i in range(len(test_images)):
-                imgplot = plt.imshow(test_images[i])
+            
 
-            # Log Status
-            tf.logging.info(
-                epoch, num_epochs, n_batch, num_batches,
-                d_error, g_error, d_pred_real, d_pred_fake
-            )
+#             # Log Status
+#             tf.logging.info(
+#                 epoch, num_epochs, n_batch, num_batches,
+#                 d_error, g_error, d_pred_real, d_pred_fake
+#             )
+    #create directory to store images after every training step
+	if not os.path.exists(epoch):
+    	os.makedirs(epoch)
+
+	#print images to folders
+	Z_batch_test = images_to_vectors(test_negatives[round(epoch / 1000)].permute(0, 2, 3, 1).numpy())
+
+    # Generate images from test noise
+    test_images = session.run(G_sample, feed_dict={Z: Z_batch_test})
+    test_images = vectors_to_images(test_images)
+
+    # Log Images
+    for i in range(10):
+    	imsave(epoch + "/" + i + ".png", test_images[i])
+
+    g_errors.append(g_error)
+    d_errors.append(d_error)
+    
+#print generator errors to csv file
+g_errors = np.array(g_errors)
+np.savetxt("generator.csv", g_errors, delimiter=",")
+
+#print discriminator errors to csv file
+d_errors = np.array(d_errors)
+np.savetxt("discriminator.csv", d_errors, delimiter=",")
+
